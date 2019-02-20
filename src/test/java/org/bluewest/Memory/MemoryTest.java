c2a099e503c548d031f9dca84f8f60e2b067a095
Werner Steinbrenne <werner.steinbrenner@rise-world.com>
2019-02-20 13:18:30 +0100

    Converted project to maven default structure

---
+++ src/test/java/org/bluewest/Memory/MemoryTest.java
@@ -0,0 +1,59 @@
+package org.bluewest.Memory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryTest {
+
+	@Test
+	void testEngineUtilRandGetInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayAddElementStringArrayString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayAddElementIntArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayAddElementIntArrayArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayAddElementStringArrayArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayRemoveByIndexStringArrayIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayRemoveByIndexIntArrayIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArrayContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArraysGetValueIndecesIntArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEngineUtilArraysGetValueIndecesStringArrayString() {
+		fail("Not yet implemented");
+	}
+
+}
